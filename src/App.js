20a3c637a203549b7d3ffb73cf1a70b674ad33ef
AKHIL NAIR <akhil9167782208@gmail.com>
2020-08-18 21:36:43 +0530

    components part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <div className = 'container'>
           <NavbarBrand href='/'>con fusion</NavbarBrand>
         </div>
-      </Navbar>    
+      </Navbar>   
+      <Menu/> 
     </div>
   );
 }
