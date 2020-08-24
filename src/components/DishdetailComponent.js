fd9a06ca650f98162bbd5255ad1968cd63ff395e
AKHIL NAIR <akhil9167782208@gmail.com>
2020-08-24 09:00:16 +0530

    small error covered

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -32,7 +32,7 @@ class Dishdetail extends Component{
                                 <li key={comment.id} >
                                     {comment.comment}
                                     <br /><br />
-                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                                    -- {comment.author}, {comment.date}
                                     <br /><br />
                                 </li>
                             );
