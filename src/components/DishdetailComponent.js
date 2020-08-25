c424ec64f628affa8992e534116455a1104ea22a
AKHIL NAIR <akhil9167782208@gmail.com>
2020-08-25 20:28:37 +0530

    maincomponent created

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -32,7 +32,7 @@ class Dishdetail extends Component{
                                 <li key={comment.id} >
                                     {comment.comment}
                                     <br /><br />
-                                    -- {comment.author}, {comment.date}
+                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                                     <br /><br />
                                 </li>
                             );
@@ -69,14 +69,16 @@ class Dishdetail extends Component{
     render(){
         if(this.props.dish){
             return(
-                <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments) }
-                        </div>
-                </div>
+                <div className="container">
+                    <div className="row">
+                            <div className="col-12 col-md-5 m-1">
+                                {this.renderDish(this.props.dish)}
+                            </div>
+                            <div className="col-12 col-md-5 m-1">
+                                {this.renderComments(this.props.dish.comments) }
+                            </div>
+                    </div>
+                </div>    
             );
         }
         else{
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,52 +2,26 @@ import React,{Component} from 'react';
 
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
-import Dishdetail from './DishdetailComponent';
 
-    
- 
-    
-    
     class Menu extends Component {
     
-        constructor(props) {
+       /* constructor(props) {
             super(props);
     
-            this.state = {
-                selectedDish: null
-            }
         }
     
-        onDishSelect(dish) {
+       /* onDishSelect(dish) {
             this.setState({ selectedDish: dish});
-        }
-    
-       /* renderDish(dish) {
-            if (dish != null){
-                return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                          <CardTitle>{dish.name}</CardTitle>
-                          <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            }
-            else{
-                return(
-                    <div></div>
-                );
-            }
         }*/
     
+      
         render() {
             
                 const menu = this.props.dishes.map((dish) => {
                     return (
                     <div  className="col-12 col-md-5 m-1">
                         <Card key={dish.id}
-                        onClick={() => this.onDishSelect(dish)}>
+                        onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -63,11 +37,6 @@ import Dishdetail from './DishdetailComponent';
                         <div className="row">
                             {menu}
                         </div>
-                        
-                                
-                        <Dishdetail dish={this.state.selectedDish}/>
-                            
-                        
                     </div>
                 );
             
