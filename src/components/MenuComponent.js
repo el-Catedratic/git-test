44102bd6424b0da7101730c104d6f7dd6a1be03d
AKHIL NAIR <akhil9167782208@gmail.com>
2020-09-03 21:37:41 +0530

    singlepageapp

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Card, CardImg, CardImgOverlay,
+        CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-
-    function RenderMenuItem({dish, onClick}){
-        return(
-            <card onClick={()=> onClick(dish.id)}>
+function RenderMenuItem ({dish, onClick}) {
+    return (
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
-            </card>
-        )
-    }
+            </Link>
+        </Card>
+    );
+}
     
       
     const Menu = (props) =>{   
@@ -20,7 +22,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
         const menu = props.dishes.map((dish) => {
             return (
             <div  className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick}/>
+                <RenderMenuItem dish={dish} />
             </div>
             );
         });
@@ -28,6 +30,16 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
 
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
                 <div className="row">
                     {menu}
                 </div>
@@ -42,4 +54,3 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     
      
 export default Menu;
- 
